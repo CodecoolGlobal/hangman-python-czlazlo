675c1d4a2675becb753af9ea53125d5165219d46
Laszlo Czinege <czinege.laszlo3@gmail.com>
2022-04-07 18:17:57 +0200

    Hopefully last version update

--- hangman.py
+++ hangman.py
@@ -1,6 +1,6 @@
-from pickle import FALSE
 import random
 #lists
+import sys
 countries = []
 easy_words=[]
 medium_words = []
@@ -57,6 +57,7 @@ HANGMAN = ['''
       |
 =========''']
 #words
+
 words = open("/home/lazlo/CC/Second week/hangman-python-czlazlo/countries-and-capitals.txt","r")
 for lines in words.readlines():
     countries.append(lines.split(" | ")[0])
@@ -76,28 +77,26 @@ for country in countries:
     hard_words.append(country)
 
 print("Welcome to hangman!")
-difficulty = input('Choose a level - 1 (7 lives, easy words), 2 (5 lives, medium words ) or 3 (3 hard words)')
+difficulty = input('Choose a level - 1 , 2  or 3 ')
+if (difficulty == "quit"):
+  print("Goodbye")
+  sys.exit()
 print(f"Difficulty level {difficulty}") 
 if difficulty == '1':
     max_wrong = 7
     country= random.choice(easy_words)
     dashed_out_word = "_" * len(country)
     print(country)
-    print(dashed_out_word)
 #Difficulty of word lvl-easy
 elif difficulty == '2':
     max_wrong = 5
     country= random.choice(medium_words)
     dashed_out_word = "_" * len(country)
-    print(country)
-    print(dashed_out_word)
 #Difficulty of word lvl-medium
 else:
     max_wrong = 3
     country = random.choice(hard_words)
     dashed_out_word = "_" * len(country)
-    print(country)
-    print(dashed_out_word)
 #Difficulty of word lvl- hard
 
 #itt meg jelolom a random szonak a hosszat underscore(_)-ba
@@ -107,7 +106,12 @@ while wrong_guesses < max_wrong:
     print("\n So far, the word is: ", dashed_out_word)
     is_valid_input= False
     while not is_valid_input:
+      new_dashed_out_word = ""
       guess= input("\n Please, enter your letter guess:")
+      if (guess == "quit"):
+        print("Goodbye")
+        sys.exit()
+      guess= guess.lower()
       if  len(guess) != 1:
         print("please input a letter")
       elif not guess.isalpha():
@@ -117,26 +121,23 @@ while wrong_guesses < max_wrong:
       else: 
         is_valid_input = True
     used_letters.add(guess)
-    if guess not in country:
+    if guess not in str(country).lower():
         print(HANGMAN[wrong_guesses])
-        guess= guess.upper() 
+        guess= guess.lower() 
         print("Sorry, that was incorrect")
     #increase the number of incorrect by 1
         wrong_guesses += 1
-    if wrong_guesses == max_wrong:
-      print("You got hanged :(")
-      print("The correct word was: ",country)
-
-   
     else: 
         print("You have guessed correctly")
-        new_dashed_out_word = ""
         for i in range(len(country)):
-          if country[i] == guess:
+          if str(country[i]).lower() == guess:
             new_dashed_out_word += guess
           else:
             new_dashed_out_word += dashed_out_word[i]
         dashed_out_word = new_dashed_out_word
-    if new_dashed_out_word == country:
+    if wrong_guesses == max_wrong:
+      print("You got hanged :(")
+      print("The correct word was: ",country)
+    if new_dashed_out_word == str(country).lower():
       print("You won!")
       break
